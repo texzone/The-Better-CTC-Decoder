519731613623f4639969c013ae613e21183ab3bd
texzone <ali.elgabri@gmail.com>
2021-06-20 18:33:32 -0400

    update

--- setup.sh
+++ setup.sh
@@ -7,14 +7,8 @@ fi
 
 if [ ! -d openfst-1.6.3 ]; then
     echo "Download and extract openfst ..."
-    sudo apt install graphviz
-    wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.8.1.tar.gz
-    tar -xzvf openfst-1.8.1.tar.gz
-    cd openfst-1.8.1/
-    ./configure --enable-far=true
-    make -j4
-    sudo make install
-    echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib" >> ~/.bashrc
+    wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.3.tar.gz
+    tar -xzvf openfst-1.6.3.tar.gz
     echo -e "\n"
 fi
 
